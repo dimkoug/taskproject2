dc503a007f85e2717d75235a098f33b90287ba3e
Dimitris Kougioumtzis <dimkoug@gmail.com>
2025-01-02 09:25:52 +0200

    wip

--- taskproject2/projects/models.py
+++ taskproject2/projects/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+from django.db.models import Min, Max
 # Create your models here.
+from django.core.exceptions import ValidationError
 from core.models import Timestamped
 
 class Category(Timestamped):
@@ -53,6 +54,32 @@ class Task(Timestamped):
     def duration(self):
         return (self.end_date - self.start_date).days
 
+    @property
+    def early_start(self):
+        # Compute ES based on predecessors
+        if self.predecessors.exists():
+            return max(predecessor.to_task.early_finish for predecessor in self.predecessor_tasks.all())
+        return 0
+
+    @property
+    def early_finish(self):
+        return self.early_start + self.duration
+
+    @property
+    def late_finish(self):
+        # Compute LF based on successors
+        if self.successor_tasks.exists():
+            return min(successor.from_task.late_start for successor in self.successor_tasks.all())
+        return self.project.tasks.aggregate(Max('end_date'))['end_date__max']
+
+    @property
+    def late_start(self):
+        return self.late_finish - self.duration
+
+    @property
+    def slack(self):
+        return self.late_start - self.early_start
+
 
 class Predecessor(Timestamped):
     FS, SS, FF ,SF = range(0,4)
@@ -73,6 +100,33 @@ class Predecessor(Timestamped):
         indexes = [
             models.Index(fields=['from_task', 'to_task']),
         ]
+    def clean(self):
+        super().clean()
+        # Ensure tasks are not the same
+        if self.from_task == self.to_task:
+            raise ValidationError("A task cannot be its own predecessor.")
+
+        # Validate based on start_type
+        if self.start_type == self.FS:
+            # Finish-to-Start: `to_task` start_date must be after `from_task` end_date
+            if self.to_task.start_date <= self.from_task.end_date:
+                raise ValidationError("For Finish-to-Start, the successor task must start after the predecessor task finishes.")
+        elif self.start_type == self.SS:
+            # Start-to-Start: `to_task` start_date must be after or equal to `from_task` start_date
+            if self.to_task.start_date < self.from_task.start_date:
+                raise ValidationError("For Start-to-Start, the successor task must start on or after the predecessor task starts.")
+        elif self.start_type == self.FF:
+            # Finish-to-Finish: `to_task` end_date must be after or equal to `from_task` end_date
+            if self.to_task.end_date < self.from_task.end_date:
+                raise ValidationError("For Finish-to-Finish, the successor task must finish on or after the predecessor task finishes.")
+        elif self.start_type == self.SF:
+            # Start-to-Finish: `to_task` end_date must be after `from_task` start_date
+            if self.to_task.end_date <= self.from_task.start_date:
+                raise ValidationError("For Start-to-Finish, the successor task must finish after the predecessor task starts.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()  # Ensures `clean()` is called before saving
+        super().save(*args, **kwargs)
 
 class CPMReport(Timestamped):
     name = models.CharField(max_length=255)
